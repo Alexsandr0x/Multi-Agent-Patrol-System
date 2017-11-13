330b4c7187691e3f3eca78be9c6e08e970b5b1f0
Alexsandro <alexsandro2134@gmail.com>
2017-11-13 20:29:55 -0200

    Add pom.xml

--- src/main/java/simpleModel/PatrolMain.java
+++ src/main/java/simpleModel/PatrolMain.java
@@ -2,6 +2,8 @@ package simpleModel;
 
 import Objects.GridMap;
 import Objects.PatrolAgent;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
 
 /**
  * Created by Alexsandr0x.
@@ -9,7 +11,10 @@ import Objects.PatrolAgent;
 public class PatrolMain {
     private static final int GRID_SIZE = 30;
 
-    public static void main(String[] args) {
+    static ContainerController containerController;
+    static AgentController agentController;
+
+    public static void main(String[] args) throws InterruptedException  {
 
         GridMap map = new GridMap(GRID_SIZE);
         PatrolAgent agent = new PatrolAgent(map);
