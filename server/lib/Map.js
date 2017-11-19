8cd8aa341491f398fe395c3a0e027a7eec3af712
Alexsandro <alexsandro2134@gmail.com>
2017-11-19 14:32:17 -0200

    Create properly map object, first methods working :)

--- server/lib/Map.js
+++ server/lib/Map.js
@@ -1,23 +1,58 @@
-const fs = require('fs')
+const fs = require('fs');
+
+var rawMap = [];
+var availableNodes = [];
 
 module.exports = class Map {
-
 	constructor(file_name) {
-		this.file_path = __dirname + '/maps/' + file_name
-		this.map = []
-		let raw_map = []
-		this.map = fs.readFile(this.file_path, function (err, fileData) {
-			let map = fileData.toString().split('\n');
-			map = raw_map.map(function(item) {
-				return item.toString().split(',').map(function(item) {
-					return {'phero': item}
-				});
-			});
-			return map
-		});
-		console.log(this.map);
-		// this.width = this.map.length;
-		// console.log(this.width)
-		//this.height = this.map[0].length;
+		this.file_path = __dirname + '/maps/' + file_name;
+        let fileData = fs.readFileSync(this.file_path, 'utf-8');
+        rawMap = fileData.toString().split('\n');
+
+        rawMap.map(function(item) {
+            return item.toString().split(',');
+        });
+
+        rawMap.forEach(function (item, i) {
+            rawMap[i] = item.split(',').map(Number);
+            rawMap[i].forEach(function (node, j) {
+                if(node != -1){
+                    availableNodes.push({x: i, y: j})
+                }
+            })
+        });
+
+        console.log(rawMap);
 	}
-}
+
+	applyPhero(x, y, phero) {
+	    if (rawMap[x][y] == -1) {
+	        return null
+        } else if (rawMap[x][y] - phero < 0) {
+            rawMap[x][y] = 0
+        } else {
+            rawMap[x][y] += phero
+        }
+    }
+
+    decayMapPhero(phero_decay) {
+        rawMap.forEach(function(row, x) {
+            row.forEach(function (item, y) {
+                applyPhero(x, y, phero_decay)
+            });
+        });
+    };
+
+	getNeighbours(x, y) {
+	    let _response = {};
+	    if(x != 0) _response.up= rawMap[x][y - 1];
+	    if(x < rawMap[0].length) _response.up= rawMap[x][y + 1];
+	    if(y !=0) _response.left = rawMap[x - 1][y];
+        if(y < rawMap.length) _response.right = rawMap[x + 1][y];
+        return _response
+    }
+
+	getRandomLocation() {
+        return availableNodes[Math.floor(Math.random() * availableNodes.length)];
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
-var map		   = require('./lib/Map.js')	 
+var map		   = require('./lib/Map.js')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,11 +13,11 @@ var router = express.Router();
 var map = new map('plain_map.csv');
 
 router.get('/random_location', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json(map.getRandomLocation());
 });
 
 router.get('/neighbours', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'hooray! welcome to our api!' });
 });
 
 router.get('/', function(req, res) {
@@ -27,4 +27,3 @@ router.get('/', function(req, res) {
 app.use('/api', router);
 
 app.listen(port);
-console.log('Magic happens on port ' + port);
